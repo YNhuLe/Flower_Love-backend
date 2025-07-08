ddc87fa68389f7b64db4d6aff152a2040a626749
NhuYLe <lenhuy10011996@gmail.com>
2025-07-08 17:14:57 -0400

    add the routes, controllers and API call for all plants

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import plantsRoute from "../routes/plants-routes";
 const app = express();
 app.use(cors());
 const port = process.env.PORT ?? 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from TypeScript + Express!");
-});
+app.use("/", plantsRoute);
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello from TypeScript + Express!");
+// });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
